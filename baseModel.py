bd41e96ae07501ff7d0e4ca22fdd3477bc964744
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-03-03 15:56:51 +0530

    updated the api routes

--- baseModel.py
+++ baseModel.py
@@ -3,3 +3,6 @@ from pydantic import BaseModel
 class KeyValueStore(BaseModel):
     key: str
     value: str
+
+class UpdateKeyValue(BaseModel):
+    new_value: str
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from baseModel import KeyValueStore
+from baseModel import KeyValueStore, UpdateKeyValue
 
 app = FastAPI()
 
@@ -11,7 +11,7 @@ async def get_home_detail():
 
 @app.post("/set/")
 async def set_key_value(key_value: KeyValueStore):
-    key_value_store[key_value.key] = key_value.key
+    key_value_store[key_value.key] = key_value.value
     return {"message": "Key-Value pair stored successfully !"}
 
 @app.get("/get/{key}")
@@ -19,6 +19,14 @@ async def get_value(key: str):
     value = key_value_store.get(key)
     if value is not None:
         return {"Key": key, "Value": value}
+    else:
+        raise HTTPException(status_code=404, detail="Key not found !")
+    
+@app.put("/update/{key}")
+async def update_value(key: str, update_data: UpdateKeyValue):
+    if key in key_value_store:
+        key_value_store[key] = update_data.new_value
+        return {"message": f"Value for key '{key}' updated successfully !"}
     else:
         raise HTTPException(status_code=404, detail="Key not found !")
 
